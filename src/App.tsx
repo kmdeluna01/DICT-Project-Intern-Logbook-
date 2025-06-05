7ef6bfaf4c2ea59865f0d7a779f6e04f030bacc8
Mikko <kmdeluna01@gmail.com>
2025-06-05 18:46:25 +0800

    Used Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./pages/Admin";
 import Appointment from "./pages/Appointment";
 import NotFound from "./pages/NotFound";
@@ -20,7 +20,7 @@ const App = () =>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin/>}/>
@@ -34,7 +34,7 @@ const App = () =>
           <Route path="/guests" element={<NotFound/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
